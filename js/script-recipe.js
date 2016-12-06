370c81666b9acd9c6a5bf6458114c508117a2bca
vaRinia <varinia.ep@gmail.com>
2016-12-06 11:15:13 -0600

    etapa9

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -5,7 +5,12 @@ $(document).ready(function(){
 
 $(".js-show-make").click(function() {
 	$(".page").addClass("make");
+	$(".js-show-recipe").removeClass("active");
+	$(".js-show-make").addClass("active");
 });
 $(".js-show-recipe").click(function() {
 	$(".page").removeClass("make");
+	$(".js-show-make").removeClass("active");
+	$(".js-show-recipe").addClass("active");
+	
 });
