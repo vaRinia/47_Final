d13080a0def78823ee0b68d495742a10fbb99afc
vaRinia <varinia.ep@gmail.com>
2016-12-06 11:08:55 -0600

    etapa8

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 	console.log("Hola Irving")
   	$(".js-menu").hide();
+});
 
+$(".js-show-make").click(function() {
+	$(".page").addClass("make");
+});
+$(".js-show-recipe").click(function() {
+	$(".page").removeClass("make");
 });
