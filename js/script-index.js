169fcdc1d482c74bdcac57567ba7ab04e95bbb73
vaRinia <varinia.ep@gmail.com>
2016-12-06 10:40:18 -0600

    etapa4 :)

--- js/script-index.js
+++ js/script-index.js
@@ -16,19 +16,26 @@ function renderHighlightedRecipes(recipesArray) {
 	$(recipesArray).each(function(index){
 		if(recipesArray[index].hasOwnProperty("highlighted") && recipesArray[index]["highlighted"] === true) {
 			renderRecipe(index);
-			console.log(index);
 		}
 	})
 }
 
 /*
-* Función que se encarga de pintar UNA recetas que tenga 
+* Función que se encarga de pintar UNA receta que tenga 
 * marcado el atributo "highlighted" como TRUE
 * Aqui se tiene que crear el HTML que esta en el 
 * archivo "templates/templates-recipe.html"
 */
 function renderRecipe(recipe) {
 	console.log('Voy a pintar la receta: ', recipe);
+	$("<a class='item-recipe' href='#'><span class='attribution'><span class='title-recipe'>TITULO DE LA RECETA</span><span class='metadata-recipe'><span class='author-recipe'> NOMBRE DEL AUTOR DE LA RECETA</span><span class='bookmarks-recipe'><span class='icon-bookmark'></span></span></span></span><img src='http'/></a>").appendTo(".list-recipes");
+	$(".list-recipes img").last().attr("src", "img/recipes/320x350/" + recipesArray[recipe].name +  ".jpg");
+	$(".title-recipe").last().text(recipesArray[recipe].title);
+	$(".author-recipe").last().text(recipesArray[recipe].source.name);
+
+	console.log(recipesArray[recipe].title);
+	
+
 }
 
 
